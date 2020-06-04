4fe6ead7c194926386069c2daaabd877ec7840a4
mlacosta <marianoacosta.003@gmail.com>
2020-06-04 12:15:11 -0300

    first version

---
+++ resources/js/main.js
@@ -0,0 +1,15 @@
+/*const template = document.getElementById("product-item");
+const templateFunction = Handlebars.compile(template);
+const element = document.getElementById("container");*/
+
+
+/*element.innerHTML = templateFunction(
+    product: [
+        {
+            name: 'Department',
+            review: 'Excelent',
+            price: '$ 1652'
+        }
+    ]
+);*/
+
